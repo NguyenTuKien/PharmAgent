df387c4d3a3d8a080eec7bfe3a97288afbf6dce9
NguyenTuKien <KienNT.B23CN465@stu.ptit.edu.vn>
2026-04-10 09:53:18 +0700

    feat: add test profile configuration and update BackendApplicationTests for improved testing setup

--- backend/src/test/java/ct01/n07/backend/BackendApplicationTests.java
+++ backend/src/test/java/ct01/n07/backend/BackendApplicationTests.java
@@ -2,12 +2,13 @@ package ct01.n07.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Thêm dòng này
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
